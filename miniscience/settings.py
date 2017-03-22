00c9ba6482dc8967b95b4ac1fd22e76e8ca86c60
Fred <frederic.fyfer@gmail.com>
2017-03-22 11:20:15 +0100

    All any request to API

--- miniscience/settings.py
+++ miniscience/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAdminUser',
+        'rest_framework.permissions.AllowAny',
     ],
     'PAGE_SIZE': 10
 }
