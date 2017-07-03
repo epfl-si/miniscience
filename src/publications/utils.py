23e9d4ba3ca51e1a6d8bff11da075c0ce14150ee
Fred <frederic.fyfer@gmail.com>
2017-07-03 13:46:40 +0200

    Add WoS parser

--- src/publications/utils.py
+++ src/publications/utils.py
@@ -1,6 +1,12 @@
 import logging
 
+from wos import WosClient
+import wos.utils
+import xml.etree.ElementTree as ET
+
 from .models import Publication, Author
+
+logger = logging.getLogger(__name__)
 
 try:
     # python 3
@@ -12,7 +18,7 @@ except ImportError:
 
 def do_request(url):
     """read content from given url, which can be either an http or file one"""
-    logging.info("Fetching %s", url)
+    logger.info("Fetching %s", url)
     content = urlopen(url)
 
     old_id = 0
@@ -74,4 +80,66 @@ def do_request(url):
 
         old_id = current_id
 
-    logging.info("Fetched %d records", count)
+    logger.info("Fetched %d records", count)
+
+def parse_wos(req):
+    with WosClient(lite=True) as client:
+        res = wos.utils.query(client, req, count=2, limit=1)
+
+    print(res)
+
+    root = ET.fromstring(res)
+
+    for title in root.iter('title'):
+        title = title[1].text
+
+    authors_list = []
+    for authors in root.iter('authors'):
+        for author in authors:
+            if author.text != 'Authors':
+                comma_pos = author.text.find(',')
+                a, created = Author.objects.get_or_create(
+                    name=author.text[comma_pos+2:],
+                    surname=author.text[:comma_pos]
+                )
+                authors_list.append(a)
+
+
+    for source in root.iter('source'):
+        if source[0].text == 'Published.BiblioYear':
+            year = int(source[1].text)
+
+    for other in root.iter('other'):
+        if other[0].text == 'Identifier.Xref_Doi':
+            doi = other[1].text
+
+    p = Publication(title=title, pub_date=year, doi=doi)
+    p.save()
+
+    for author in authors:
+        p.authors.add(author)
+
+    print('Title: %s\nYear: %d\nDOI: %s\nAuthors: %s' % (title, year, doi, authors_list))
+
+    logger.info("Added publication: %s (%s)" % (title, doi))
+
+# Returns:  -1  if publication already exists
+#           0   if not sure
+#           1   if publication doesn't exist
+def check(publication):
+    if hasattr(publication, 'doi'):
+        doi_exists = True
+        if doi_exists:
+            return -1
+
+    if hasattr(publication, 'title'):
+        title_exists = True
+        authors_same = True
+
+        if title_exists:
+            if authors_same:
+                return -1
+            else:
+                return 0
+
+    return 1
